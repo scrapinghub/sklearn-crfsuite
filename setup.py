e18a02ae11b61f703c0ec460c44cfdd90db00591
Adrián Chaves <adrian@chaves.io>
2024-06-18 13:07:19 +0200

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="sklearn-crfsuite",
-    version="0.4.0",
+    version="0.5.0",
     author="Mikhail Korobov",
     author_email="kmike84@gmail.com",
     license="MIT license",
